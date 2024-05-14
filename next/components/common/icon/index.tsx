246733e17d32a42944f4a0b19e6b942f4ebbdcd6
Reza_ashrafi <ashrafimiireza98@gmail.com>
2024-05-14 21:02:54 +0330

    develop sample page for sign-in page!

--- next/components/common/icon/index.tsx
+++ next/components/common/icon/index.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 
 import { Icons } from "./icons";
-import {width} from "@mui/system";
 
 type IconComponentType = {
   color?: string;
@@ -13,37 +12,7 @@ type IconComponentType = {
 export default function Icon(props: IconComponentType) {
   const { name, color, size } = props;
   const iconsMap = {
-    warning: <Icons.Warning {...props} />,
-    exit: <Icons.Exit {...props} />,
-    more: <Icons.More {...props} />,
-    "wallet-outline": <Icons.WalletOutline {...props} />,
-    setting: <Icons.Setting {...props} />,
-    bell: <Icons.Bell {...props} />,
-    user: <Icons.User {...props} />,
-    dashboard: <Icons.Dashboard {...props} />,
-    bcard: <Icons.BCard {...props} />,
-    chart: <Icons.Chart {...props} />,
-    history: <Icons.History {...props} />,
-    friends: <Icons.Friends {...props} />,
-    "chevron-up": <Icons.ChevronUp {...props} />,
-    "account-management": <Icons.AccountManagement {...props} />,
-    trash: <Icons.Trash {...props} />,
-    "star-fill": <Icons.StarFill {...props} />,
-    plus: <Icons.Plus {...props} />,
-    menu: <Icons.Menu {...props} />,
-    export: <Icons.Export {...props} />,
-    import: <Icons.Import {...props} />,
-    eye: <Icons.Eye {...props} />,
-    search: <Icons.Search {...props} />,
-    "arrow-gain": <Icons.ArrowGain {...props} />,
-    "info": <Icons.Info {...props} />,
-    "tick": <Icons.Tick {...props} />,
-    "close-filled": <Icons.CloseFilled {...props} />,
-    "chevron": <Icons.ChevronRight {...props} />,
-    "copy-icon": <Icons.CopyIcon {...props} />,
-    "arrow": <Icons.Arrow {...props} />,
-    "transaction": <Icons.Transaction {...props} />,
-    "logout": <Icons.Logout {...props} />,
+    
   };
   return <p style={{width:size,height:"auto",display:"flex"}}>{iconsMap[name]}</p>;
 }
--- next/components/pages/auth/sign-in/page.tsx
+++ next/components/pages/auth/sign-in/page.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./page.module.scss";
-import { Layout } from "@/components/common";
+import { Button, Input, Layout } from "@/components/common";
+import { pageI18 } from "./page_i18";
+import Link from "next/link";
 
 export default function SignIn() {
+  const [checkboxValue, setCheckboxValue] = useState(false);
   return (
     <Layout>
       <div className={`${styles["wrapper"]}`}>
-        
+        <form
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => e.preventDefault()}
+        >
+          <div className={styles["form-wrapper"]}>
+            <p className={styles["title"]}>
+              {pageI18["Sign in to your account"]}
+            </p>
+            <label htmlFor="email" className={styles["email"]}>
+              {pageI18.Email}
+              <input id="email" type="email" />
+            </label>
+            <label htmlFor="password" className={styles["email"]}>
+              {pageI18.Password}
+              <input id="password" type="password" />
+            </label>
+            <div className={styles["options"]}>
+              <Input
+                type="checkbox"
+                label={pageI18["Remember me"]}
+                checked={checkboxValue}
+                onChange={() => setCheckboxValue(!checkboxValue)}
+                name="remember"
+              />
+              <Link href="/forget-password" className={styles["forget"]}>
+                {pageI18["Forget password?"]}
+              </Link>
+            </div>
+            <Button
+              className={styles["button"]}
+              title={pageI18["Sign in"]}
+              onClick={() => {}}
+            />
+          </div>
+          <div className={styles["new-user"]}>
+            {pageI18["New user?"]}
+            <Link href="/auth/sign-up" className={styles["register"]}>
+              {pageI18["Register"]}
+            </Link>
+          </div>
+        </form>
       </div>
     </Layout>
   );
