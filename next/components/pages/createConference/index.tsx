87c7d93cf249609030e51a3d4d7869a152d0864d
Reza_ashrafi <ashrafimiireza98@gmail.com>
2024-05-27 22:58:14 +0330

    add some change on create page design

--- next/components/pages/createConference/index.tsx
+++ next/components/pages/createConference/index.tsx
@@ -1,26 +1,50 @@
-import { Layout, ConfSidebar, Icon } from "@/components/common";
+import { Button, Layout } from "@/components/common";
 import styles from "./index.module.scss";
 import { useState } from "react";
 import GetMeetingDate from "./steps/getMeetingDate";
 import GetMeetingName from "./steps/getMeetingName";
+import { pageI18 } from "./i18";
 
 export default function CreateConference() {
   const [page, setPage] = useState("getName");
+  const [formBody, setFormBody] = useState({
+    name: "",
+    date: "",
+  });
   const railTransforms = {
     getName: styles.getName,
-    getOptions: styles.getOptions,
+    getDate: styles.getDate,
   };
+
   return (
     <Layout>
       <div className={styles.wrapper}>
-        <div className={`${styles.rail} ${railTransforms[page]}`}>
-          <GetMeetingName active={page === "getName"} />
-          <GetMeetingDate active={page === "getOptions"} />
-        </div>
         <div
-          className={styles.actionButtons}
-          onClick={() => setPage("getOptions")}
-        ></div>
+          className={`${styles.rail} ${railTransforms[page as keyof Object]}`}
+        >
+          <GetMeetingName
+            active={page === "getName"}
+            name={formBody.name}
+            setName={(val: string) => setFormBody({ ...formBody, name: val })}
+          />
+          <GetMeetingDate
+            date={formBody.date}
+            setDate={(val: string) => setFormBody({ ...formBody, date: val })}
+            active={page === "getDate"}
+          />
+        </div>
+        <div className={styles.actionButtons}>
+          <Button
+            className={styles.next}
+            title={pageI18["Next"]}
+            onClick={() => setPage("getDate")}
+          />
+          <Button
+            className={styles.prev}
+            title={pageI18["Prev"]}
+            onClick={() => setPage("getName")}
+          />
+        </div>
       </div>
     </Layout>
   );
