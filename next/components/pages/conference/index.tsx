c32adc0031ae5f447dbfaff6bfe493c6b1175054
Reza_ashrafi <ashrafimiireza98@gmail.com>
2024-05-30 20:45:42 +0330

    install Agroa

--- next/components/pages/conference/index.tsx
+++ next/components/pages/conference/index.tsx
@@ -1,10 +1,13 @@
 import { Layout, ConfSidebar } from "@/components/common";
 import styles from "./index.module.scss";
 import ConfNavbar from "./layouts/ConfNavbar";
-import Participants from "./layouts/Participants";
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
 import RequestToJoin from "./layouts/RequestToJoin";
 import TaskList from "./layouts/TaskList";
 import ChatRoom from "./layouts/ChatRoom";
+
+const Participants = dynamic(() => import('./layouts/Participants'), { ssr: false });
 
 export default function Conference() {
   return (
--- next/components/pages/conference/layouts/Participants/index.tsx
+++ next/components/pages/conference/layouts/Participants/index.tsx
@@ -1,12 +1,20 @@
+"use client";
 import React from "react";
 import styles from "./index.module.scss";
 import { ConfMainView, ConfParticipantList } from "@/components/common";
+import AgoraRTC, { AgoraRTCProvider, useRTCClient } from "agora-rtc-react";
 
 export default function Participants() {
+  const client = useRTCClient(
+    AgoraRTC.createClient({ codec: "vp8", mode: "rtc" })
+  );
+
   return (
-    <div className={styles.wrapper}>
-      <ConfMainView />
-      <ConfParticipantList />
-    </div>
+    <AgoraRTCProvider client={client}>
+      <div className={styles.wrapper}>
+        <ConfMainView />
+        <ConfParticipantList />
+      </div>
+    </AgoraRTCProvider>
   );
 }
